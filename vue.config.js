f5f8332dd99e097b6460704f20f1e34b58b30d60
MSI <169madman@gmail.com>
2021-03-07 22:10:54 +0800

    init

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    pages: {//配置多页面入口        
+        // login: {          
+        //   entry: 'src/login/login.main.js',          
+        //   template: 'public/login.html',        
+        // },        
+        index: {          
+          entry: 'src/main.ts',          
+          template: 'public/index.html',        
+        },    
+      }
+}
