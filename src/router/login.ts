f586c0721e1bd7137fc273c9f54b7f452cbb88c3
CFL <169madman@gmail.com>
2021-03-19 18:24:24 +0800

    update

--- src/router/login.ts
+++ src/router/login.ts
@@ -1,22 +1,33 @@
 
 /* login.html 路由  */
+import { createRouter, createWebHistory, RouteRecordRaw, createWebHashHistory } from 'vue-router';
+import Login from '@/pages/login/Login.vue';
 export const login = [
     {
-        path:'/',
-        name:'',
-        redirect:'/ui/login/QRCode'
+        path:'/login',
+        name:"登录页",
+        children:[
+            {
+                path:'/ui/login/QRCode',
+                name:'QRCode',
+                component: () => import("@/pages/login/components/QRCode.vue"),
+            },
+            {
+                path:'/ui/login/Account',
+                name:'Account',
+                component: () => import("@/pages/login/components/Account.vue"),
+            }
+        ]
     },
-    {
-        path:'/ui/login/QRCode',
-        name:'QRCode',
-        component: () => import("@/pages/login/components/QRCode.vue"),
-    },
-    {
-        path:'/ui/login/Account',
-        name:'Account',
-        component: () => import("@/pages/login/components/Account.vue"),
-    }
 ];
+
+const router = createRouter({
+    // history: createWebHistory(process.env.BASE_URL),
+    history: createWebHashHistory(),
+    login,
+  });
+  
+  export default router;
 
 
 
--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,31 @@
 const path = require("path");
 const webpack = require('webpack')
 module.exports = {
+    // 基本路径
+    // baseUrl: '/',
+    // 输出文件目录
+    // outputDir: 'dist',
     // 关闭ESlint
     lintOnSave: false,
     pages: {//配置多页面入口        
         login: {          
           entry: 'src/pages/login/main.ts',          
-          template: 'src/pages/login/login.html',        
-        },        
+          template: 'src/pages/login/login.html',
+          filename: 'login.html',
+          title:'哈哈'        
+        },      
         index: {        
           // page 的入口文件  
           entry: 'src/pages/index/main.ts',          
           // 模板文件
           template: 'src/pages/index/index.html',        
-        },    
+        }, 
+        /* login:{
+            entry: 'src/views/login/main.ts',          
+            template: 'src/views/login/login.html',
+            filename: 'login.html',
+            title:'哈哈'
+        }  */
     },
     configureWebpack: {
          plugins: [
