608785526b0004cb5f608c2836c1ad92338c8e65
CFL <169madman@gmail.com>
2021-03-31 13:30:40 +0800

    滑块组件

---
+++ src/components/PuzzleVerify.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="puzzle-warp">
+        <div>
+            <span>完成拼图验证</span>
+            <a><span class='iconfont icon-refresh'></span>换一张</a>
+        </div>
+        <div></div>
+        <div></div>
+    </div>
+</template>
+
+<script lang='ts'>
+    import { defineComponent } from 'vue'
+    export default defineComponent ({
+
+    })
+</script>
+
+<style lang='less' scoped>
+    @import url('~@/assets/fonts/font01/iconfont.css');
+    
+</style>
