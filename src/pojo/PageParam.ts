4dbde25aea4512e9e941279d78ccad2a3e960c12
CFL <169madman@gmail.com>
2021-05-24 17:29:31 +0800

    update index

---
+++ src/pojo/PageParam.ts
@@ -0,0 +1,9 @@
+/**
+ * 页面 url上的参数对象
+ */
+export default class PageParam {
+  /**
+   * token字符串
+   */
+  token: string | undefined = undefined;
+}
--- src/utils/HtmlParamUtil.ts
+++ src/utils/HtmlParamUtil.ts
@@ -1,22 +1,45 @@
 import BindPageParam from '@/pojo/BindPageParam';
+import PageParam from "@/pojo/PageParam";
+
+
 
 /**
  * 解析绑定账号页面的参数列表
  */
-export default function resolveBindPageParam() {
-  const { search } = window.location;
+export default function resolveBindPageParam():BindPageParam {
+  const bindPageParam = new BindPageParam();
+  generateParam(bindPageParam);
+  console.log(bindPageParam);
+  return bindPageParam;
+}
+
+/**
+ * 解析页面参数
+ */
+export function resolvePageParam(): PageParam {
+  const paramObj = new PageParam();
+  generateParam(paramObj);
+  console.log(paramObj);
+  return paramObj;
+}
+
+/**
+ * 根据路径参数，返回对象键值对
+ * @param returnObj
+ */
+function generateParam(pageParamObj: PageParam | BindPageParam) {
+  let {search} = window.location;
+  // 解码
+  search = decodeURI(search);
+
   const paramStr = search.substring(1, search.length);
   const paramArr = paramStr.split('&');
 
-  const bindPageParam = new BindPageParam();
   paramArr.forEach((item) => {
     const keyValue = item.split('=');
     const [param, value] = keyValue;
 
     // @ts-ignore
-    bindPageParam[param] = value;
+    pageParamObj[param] = value;
   });
-
-  console.log(bindPageParam);
-  return bindPageParam;
 }
