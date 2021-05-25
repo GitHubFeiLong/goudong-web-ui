decdafb4ac6b9b3ce272f6b8d63741e8285b948c
CFL <169madman@gmail.com>
2021-05-25 17:01:23 +0800

    优化ts代码

--- src/pages/index/Index.vue
+++ src/pages/index/Index.vue
@@ -6,7 +6,6 @@
 import { defineComponent, onMounted } from 'vue';
 
 import { resolvePageParam } from '@/utils/HtmlParamUtil';
-import * as a from '@/utils/LocalStorageUtil';
 
 import PageParam from '@/pojo/PageParam';
 import { AUTHORIZATION } from '@/pojo/ProjectConst';
@@ -16,7 +15,7 @@ import LocalStorageUtil from '@/utils/LocalStorageUtil';
 export default defineComponent({
   setup() {
     onMounted(() => {
-      const pageParam = resolvePageParam();
+      const pageParam: PageParam = resolvePageParam();
       // 本地存储token
       if (pageParam.token) {
         LocalStorageUtil.set(AUTHORIZATION, pageParam.token);
