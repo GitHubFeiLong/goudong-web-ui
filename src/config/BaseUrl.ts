8dca319a1d8c28b17d428f1969273b31be5d776d
陈飞龙 <1696741038@qq.com>
2022-08-07 08:43:36 +0800

    还原

--- src/config/BaseUrl.ts
+++ src/config/BaseUrl.ts
@@ -1,16 +1,15 @@
 // 这里是一个默认的url，可以没有
 let gatewayUrl = '';
-
 // 根据环境设置 服务不同的地址
-switch (process.env.NODE_ENV) {
-  case 'development': // 开发环境url
-    gatewayUrl = 'http://localhost:10000/';
+switch (process.env.VUE_APP_ENV) {
+  case 'test': // 测试环境
+    gatewayUrl = 'http://192.168.74.128:10000/';
     break;
-  case 'production': // 生产环境url
+  case 'prod': // 生产环境url
     gatewayUrl = 'http://goudong.shop:10000/';
     break;
-  default:
-    console.log(process.env.NODE_ENV);
+  default: // 开发环境url
+    gatewayUrl = 'http://localhost:10000/';
     break;
 }
 
